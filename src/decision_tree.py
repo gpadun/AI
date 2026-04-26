4021b5f11c4341e950fb91fa4571b747138b51c9
guilherme padun santos <guilherme.santos@solfacil.com.br>
2026-04-26 00:56:20 -0300

    fix: adjusting decision tree logic

--- src/decision_tree.py
+++ src/decision_tree.py
@@ -5,34 +5,34 @@ class Question:
         self.column = column
         self.value = value
 
-    def match(self, example):
-        # Pega o valor da coluna específica na linha que estamos testando
+    def match(self, example): 
         val = example[self.column]
         if val >= self.value:
             return True
         else:
             return False
 
-class Leaf:
-    def __init__(self, df):
-        self.predictions = df['species'].value_counts().to_dict()
+class LeafNode:
+    def __init__(self, df, target_col):
+        self.predictions = df[target_col].value_counts().to_dict()
 
-class Decision_node:
-    def __init__(self, question, true_branch, false_branch):
+class DecisionNode:
+    def __init__(self, question, true_branch, false_branch): 
         self.question = question
         self.true_branch = true_branch
         self.false_branch = false_branch
 
 class DecisionTree:
-    def __init__(self):
+    def __init__(self, target_col):
        self.root = None
+       self.target_col = target_col
 
     def _gini(self, df):
         impurity = 1
         prob_sum = 0
-        data_lableds = df['species'].value_counts().to_list()
+        data_labels = df[self.target_col].value_counts().to_list()
         probabilities = []
-        for value in data_lableds:
+        for value in data_labels:
             probabilities.append(value/float(df.shape[0]))
         for probability in probabilities:
             probability = probability**2
@@ -55,11 +55,11 @@ class DecisionTree:
         return true_rows, false_rows
 
 
-    def _best_split(self, df):
+    def _best_split(self, df): #essa função retorna o nó raiz? de novo valor hardcodado ali
         best_gain = 0
         best_question = None
         current_impurity = self._gini(df)
-        features = df.columns.drop('species')
+        features = df.columns.drop(self.target_col)
         for column in features:
             single_values = df[column].unique()
             for val in single_values:
@@ -75,36 +75,29 @@ class DecisionTree:
     def _build_tree(self, df):
         gain, question = self._best_split(df)
         if gain == 0:
-            return Leaf(df)
+            return LeafNode(df, self.target_col)
         true_rows, false_rows = self._partition(df, question)
         true_branch = self._build_tree(true_rows)
         false_branch = self._build_tree(false_rows)
-        node = Decision_node(question, true_branch, false_branch)
+        node = DecisionNode(question, true_branch, false_branch)
         return node
 
-    def predict(self, df_teste):
-        """Recebe um DataFrame de teste e devolve uma lista com as previsões."""
-        previsoes = []
-        # Iteramos sobre cada linha do DataFrame de teste
-        for _, row in df_teste.iterrows():
-            # Pegamos o dicionário da folha
-            resultado_dict = self._classify(row, self.root)
-            # Descobrimos qual é a chave (espécie) com o maior número de votos
-            classe_vencedora = max(resultado_dict, key=resultado_dict.get)
-            previsoes.append(classe_vencedora)
-
-        return previsoes
+    def predict(self, df_test):
+        predictions = []
+        for _, row in df_test.iterrows():
+            result_dict = self._classify(row, self.root)
+            winning_class = max(result_dict, key=result_dict.get)
+            predictions.append(winning_class)
+        return predictions
 
     def _classify(self, row, node):
-        """Navega recursivamente pela árvore até achar uma folha."""
-        # Se chegamos em uma folha, devolvemos o dicionário de previsões
-        if isinstance(node, Leaf):
+        if isinstance(node, LeafNode):
             return node.predictions
-        # Se não é folha, usamos a pergunta para saber se vamos para a esquerda ou direita
+            
         if node.question.match(row):
             return self._classify(row, node.true_branch)
         else:
             return self._classify(row, node.false_branch)
 
     def fit(self, df):
-       self.root = self._build_tree(df)
+        self.root = self._build_tree(df)
